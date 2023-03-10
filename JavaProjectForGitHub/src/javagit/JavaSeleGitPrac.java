21512629070340f05ac9dab97a2f21fcb248909f
rpatil79 <rpatil79@US-FVG4XM3>
2023-03-10 13:17:30 +0530

    UpdatedCommit

--- JavaProjectForGitHub/src/javagit/JavaSeleGitPrac.java
+++ JavaProjectForGitHub/src/javagit/JavaSeleGitPrac.java
@@ -5,5 +5,7 @@ public class JavaSeleGitPrac {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Sumit!! How are You??");
+		
+		System.out.println("Enjoy your weekends Bro");
 	}
 }
